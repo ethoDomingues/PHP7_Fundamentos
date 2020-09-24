1ca979bc8a43aeec42e9ca9f28010f0efddf4742
ethoDomingues <ethodomingues3@gmail.com>
2020-09-24 18:54:04 -0300

    Finalizando Aula 5.

--- aula_05/index.php
+++ aula_05/index.php
@@ -1,2 +1,21 @@
-<?
-	
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Index</title>
+	</head>
+	<body>
+		<?php
+			// Isso é um comentário de uma linha
+			
+			/*
+				Já isso aqui,
+				é um comentário
+				de múltiplas
+				linhas
+			*/
+			echo  "Meu nome é Everton,";
+			print " E tenho 19 anos.";
+		?>
+	</body>
+</html>
