83d54b0b0aa7780f7ba1bd200d01101894f4b0e4
ethoDomingues <ethodomingues3@gmail.com>
2020-09-24 19:03:09 -0300

    Aula 06: Variáveis.

---
+++ aula_06/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Variáveis</title>
+	</head>
+	<body>
+		<?php
+			$nome = "Etho";
+			$idade = 19;
+			$altura = 1.75;
+			
+			echo "Meu nome é $nome, tenho $idade anos e tenho $altura de altura";
+		?>
+	</body>
+</html>
