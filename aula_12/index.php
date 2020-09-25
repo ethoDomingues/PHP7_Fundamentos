f2ad6a4934b7efad96c880de055e561e0c7cdf48
ethoDomingues <ethodomingues3@gmail.com>
2020-09-25 08:03:13 -0300

    Aula 12: Arrays pt.1

---
+++ aula_12/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Aula 12 - Arrays</title>
+	</head>
+	<body>
+		<?php
+
+			$carros = array("fusca","fiat147","Brasília");
+			$motos = array();
+			$motos = ["cg125","cg150","fan125"];
+			$caminhao = ["Fh400","Constellation","Scannia124"];
+			
+			$pessoas = [];
+			$pessoas += ["etho"];
+			$pessoas[10] = "maria";
+			$pessoas[2] = "cachaça";
+			
+			
+			print_r($carros);
+			echo "<br/>";
+			print_r($motos);
+			echo "<br/>";
+			print_r($caminhao);
+			echo "<br/>";
+			print_r($pessoas)
+	
+		?>
+	</body>
+</html>
